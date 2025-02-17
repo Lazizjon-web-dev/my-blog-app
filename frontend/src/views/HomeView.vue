a6187d65789d3e763331051772a591c140397d0f
Lazizjon-web-dev <142150917+Lazizjon-web-dev@users.noreply.github.com>
2025-02-17 20:36:59 +0500

    Added views

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,24 +1,6 @@
-<template>
-  <div>
-    <h1>Blog Posts</h1>
-    <ul>
-      <li v-for="post in posts" :key="post.id">{{ post.title }}</li>
-    </ul>
-  </div>
-</template>
-
-<script>
-import api from '@/services/api';
-
-export default {
-  data() {
-    return {
-      posts: [],
-    };
-  },
-  async created() {
-    const response = await api.get('/api/posts');
-    this.posts = response.data;
-  },
-};
+<script setup lang="ts">
 </script>
+
+<template>
+    <main>This is home page</main>
+</template>
