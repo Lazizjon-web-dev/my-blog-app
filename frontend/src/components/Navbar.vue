78acf086e55520515d3c85568c5ad342a6434779
Lazizjon-web-dev <142150917+Lazizjon-web-dev@users.noreply.github.com>
2025-02-23 09:58:29 +0500

    Made Navigation bar more relevant

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,72 +1,31 @@
 <template>
-    <nav class="bg-white border-gray-200 dark:bg-gray-900">
+    <nav class="bg-white border-gray-200 dark:bg-gray-900 sticky">
         <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-            <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
-                <img src="../assets/logo.svg" class="h-8" alt="Flowbite Logo" />
+            <router-link to="/" class="flex items-center space-x-3 rtl:space-x-reverse">
+                <img src="../assets/logo.svg" class="h-8" alt="Blog Logo" />
                 <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"
                     >My Blog</span
                 >
-            </a>
+            </router-link>
             <div class="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-                <button
-                    type="button"
-                    class="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
-                    id="user-menu-button"
-                    aria-expanded="false"
-                    data-dropdown-toggle="user-dropdown"
-                    data-dropdown-placement="bottom"
-                >
-                    <span class="sr-only">Open user menu</span>
-                    <img
-                        class="w-8 h-8 rounded-full"
-                        src="./docs/images/people/profile-picture-3.jpg"
-                        alt="user photo"
-                    />
-                </button>
-                <!-- Dropdown menu -->
-                <div
-                    class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow-sm dark:bg-gray-700 dark:divide-gray-600"
-                    id="user-dropdown"
-                >
-                    <div class="px-4 py-3">
-                        <span class="block text-sm text-gray-900 dark:text-white"
-                            >Bonnie Green</span
+                <UserDropdownMenu v-if="token"></UserDropdownMenu>
+                <ul v-else class="flex">
+                    <li>
+                        <router-link
+                            to="/login"
+                            class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                            >Login</router-link
                         >
-                        <span class="block text-sm text-gray-500 truncate dark:text-gray-400"
-                            >name@flowbite.com</span
+                    </li>
+                    <span class="text-gray-900 dark:text-white mx-2"> | </span>
+                    <li>
+                        <router-link
+                            to="/register"
+                            class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                            >Register</router-link
                         >
-                    </div>
-                    <ul class="py-2" aria-labelledby="user-menu-button">
-                        <li>
-                            <a
-                                href="#"
-                                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                                >Dashboard</a
-                            >
-                        </li>
-                        <li>
-                            <a
-                                href="#"
-                                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                                >Settings</a
-                            >
-                        </li>
-                        <li>
-                            <a
-                                href="#"
-                                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                                >Earnings</a
-                            >
-                        </li>
-                        <li>
-                            <a
-                                href="#"
-                                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                                >Sign out</a
-                            >
-                        </li>
-                    </ul>
-                </div>
+                    </li>
+                </ul>
                 <button
                     data-collapse-toggle="navbar-user"
                     type="button"
@@ -75,21 +34,7 @@
                     aria-expanded="false"
                 >
                     <span class="sr-only">Open main menu</span>
-                    <svg
-                        class="w-5 h-5"
-                        aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 17 14"
-                    >
-                        <path
-                            stroke="currentColor"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
-                            d="M1 1h15M1 7h15M1 13h15"
-                        />
-                    </svg>
+                    <HamburgerMenuIcon></HamburgerMenuIcon>
                 </button>
             </div>
             <div
@@ -100,39 +45,24 @@
                     class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
                 >
                     <li>
-                        <a
-                            href="#"
+                        <router-link
+                            to="/"
                             class="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
-                            aria-current="page"
-                            >Home</a
+                            >Home</router-link
                         >
                     </li>
                     <li>
-                        <a
-                            href="#"
+                        <router-link
+                            to="/"
                             class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                            >About</a
+                            >Categories</router-link
                         >
                     </li>
                     <li>
-                        <a
-                            href="#"
+                        <router-link
+                            to="/"
                             class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                            >Services</a
-                        >
-                    </li>
-                    <li>
-                        <a
-                            href="#"
-                            class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                            >Pricing</a
-                        >
-                    </li>
-                    <li>
-                        <a
-                            href="#"
-                            class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                            >Contact</a
+                            >About</router-link
                         >
                     </li>
                 </ul>
@@ -170,6 +100,10 @@ Copy
 </template>
 
 <script setup lang="ts">
+import { ref, computed } from "vue";
 import { useAuthStore } from "../stores/authStore";
+import HamburgerMenuIcon from "./icons/HamburgerMenuIcon.vue";
+import UserDropdownMenu from "./UserDropdownMenu.vue";
 const store = useAuthStore();
+const token = computed(() => store.token);
 </script>
