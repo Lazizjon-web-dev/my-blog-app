bb8d9c2e3f73ac4d10d3db08a19dd61ce1f37593
Lazizjon-web-dev <142150917+Lazizjon-web-dev@users.noreply.github.com>
2025-02-23 09:53:27 +0500

    Made function inputs more general and Added status checks for response

--- frontend/src/stores/authStore.ts
+++ frontend/src/stores/authStore.ts
@@ -8,17 +8,21 @@ export const useAuthStore = defineStore("auth", () => {
     const token = ref<string | null>(localStorage.getItem("token") || null);
 
     //? Actions
-    async function login(email: string, password: string) {
-        const response = await api.post("/api/auth/login", { email, password });
-        user.value = response.data.user;
-        token.value = response.data.token;
-        localStorage.setItem("token", response.data.token);
+    async function login(credentials: { email: string; password: string }) {
+        const response = await api.post("/api/auth/login", credentials);
+        console.log(response);
+        if (response.status === 200) {
+            token.value = response.data.token;
+            localStorage.setItem("token", response.data.token);
+        }
     }
-    async function register(username: string, email: string, password: string) {
-        const response = await api.post("/api/auth/register", { username, email, password });
-        user.value = response.data.user;
-        token.value = response.data.token;
-        localStorage.setItem("token", response.data.token);
+    async function register(credentials: { username: string; email: string; password: string }) {
+        const response = await api.post("/api/auth/register", credentials);
+        console.log(response);
+        if (response.status === 200) {
+            user.value = response.data.data;
+            login({ email: credentials.email, password: credentials.password });
+        }
     }
     function logout() {
         user.value = null;
