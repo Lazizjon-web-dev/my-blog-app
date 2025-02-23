081799c40e741201cae8d02e82fd0684ae8bbb90
Lazizjon-web-dev <142150917+Lazizjon-web-dev@users.noreply.github.com>
2025-02-23 10:03:02 +0500

    Added basic view for home page

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,6 +1,13 @@
 <script setup lang="ts">
+import VerticalCard from "@/components/VerticalCard.vue";
 </script>
 
 <template>
-    <main>This is home page</main>
+    <main class="">
+        <div
+            class="m-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-items-center gap-2 md:mx-32 xl:grid-cols-4 xl:mx-16"
+        >
+            <VerticalCard v-for="i in 12" :key="i" :id="i"></VerticalCard>
+        </div>
+    </main>
 </template>
