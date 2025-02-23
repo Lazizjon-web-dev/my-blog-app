118528321073917fe598082de13cc20824c4f92c
Lazizjon-web-dev <142150917+Lazizjon-web-dev@users.noreply.github.com>
2025-02-23 10:01:49 +0500

    Added a route check for showing/hiding navbar in certain pages

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from "vue-router";
+import { computed } from "vue";
+import { useRoute, RouterView } from "vue-router";
 import Navbar from "./components/Navbar.vue";
+
+const route = useRoute();
+const showNavbar = computed(() => !["login", "register"].includes(route.name?.toString() || ""));
 </script>
 
 <template>
-    <Navbar></Navbar>
+    <Navbar v-if="showNavbar"></Navbar>
     <RouterView />
 </template>
