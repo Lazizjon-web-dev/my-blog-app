a6e0eda7327b6decae84993993df25a6589a6f08
Lazizjon-web-dev <142150917+Lazizjon-web-dev@users.noreply.github.com>
2025-02-25 11:55:07 +0500

    Created User type and added logic to store, fetch and remove the auth data to local storage

--- frontend/src/stores/authStore.ts
+++ frontend/src/stores/authStore.ts
@@ -2,9 +2,28 @@ import { defineStore } from "pinia";
 import { ref } from "vue";
 import api from "@/services/api";
 
+type User = {
+    id: number;
+    email: string;
+    username: string;
+    created_at: Date;
+};
 export const useAuthStore = defineStore("auth", () => {
     //? State
-    const user = ref<Object | null>(null);
+    let id = localStorage.getItem("id");
+    let email = localStorage.getItem("email");
+    let username = localStorage.getItem("username");
+    let created_at = localStorage.getItem("created_at");
+    let tempUser: User | null = null;
+    if (id && email && username && created_at) {
+        tempUser = {
+            id: +id,
+            email,
+            username,
+            created_at: new Date(created_at),
+        };
+    }
+    const user = ref<User | null>(tempUser || null);
     const token = ref<string | null>(localStorage.getItem("token") || null);
 
     //? Actions
@@ -13,7 +32,15 @@ export const useAuthStore = defineStore("auth", () => {
         console.log(response);
         if (response.status === 200) {
             token.value = response.data.token;
+            user.value = response.data.user;
+            for (let key in response.data.user) {
+                let element = response.data.user[key];
+                localStorage.setItem(key, element);
+            }
             localStorage.setItem("token", response.data.token);
+            if (!user) {
+                const response = await api.post("/api/", token.value);
+            }
         }
     }
     async function register(credentials: { username: string; email: string; password: string }) {
@@ -27,7 +54,8 @@ export const useAuthStore = defineStore("auth", () => {
     function logout() {
         user.value = null;
         token.value = null;
-        localStorage.removeItem("token");
+        let items = ["token", "id", "email", "username", "created_at"];
+        items.forEach((item) => localStorage.removeItem(item));
     }
 
     //? Return state and actions
